ec2e1963afa4588cd4789f0afc01f000f2f42b94
Alex Minu <alexandru_minu@yahoo.com>
2018-10-14 20:44:34 +0300

    summary

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+    public Vector3 speed = new Vector3(10f,0f,0f);
+
+	void Update () {
+        transform.Rotate(speed * Time.deltaTime);
+	}
+}
